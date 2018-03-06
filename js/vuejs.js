ca5eb6a824edb861bbf001e62b90146f8a667a08
Justin Jones <justinjones00@gmail.com>
2018-03-06 13:11:18 -0600

    Done with first part.

--- js/vuejs.js
+++ js/vuejs.js
@@ -1,11 +1,10 @@
 new Vue({
   el: '#vue-app',
   data:{
-    output: "Your fave food: "
+    output: "Your fave food"
   },
   methods:{
     readRefs:function(){
-      console.log(this.$refs.input.value);
       this.output = this.$refs.input.value;
     },
   }
